5986541d800706f0c4e4c5a137d2103aa01e7a00
hkandrealaw <hkandrealaw@gmail.com>
2018-12-12 01:00:07 +1100

    Create multiple_constructors.js

---
+++ practice/JavaScript/OOP/multiple_constructors.js
@@ -0,0 +1,28 @@
+// Avoid duplication in multiple constructor functions using call or apply
+
+function Car(make, model, year){
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  // we can also set properties on the keyword this
+  // that are preset values
+  this.numWheels = 4;
+}
+
+function Motorcycle(make, model, year){
+  // using call
+  Car.call(this, make, model, year);
+  this.numWheels = 2;
+}
+
+function Motorcycle(make, model, year){
+  // using apply
+  Car.apply(this, [make, model, year]);
+  this.numWheels = 2;
+}
+
+function Motorcycle(make, model, year){
+  // even better using apply with arguments
+  Car.apply(this, arguments);
+  this.numWheels = 2;
+}
