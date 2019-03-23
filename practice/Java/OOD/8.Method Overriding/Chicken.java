a2cda9d5ca572eb0fe33e6911e24d5312eef02a9
hkandrealaw <hkandrealaw@gmail.com>
2019-03-23 16:41:05 +1100

    Add Java method overriding sample code

---
+++ practice/Java/OOD/8.Method Overriding/Chicken.java
@@ -0,0 +1,13 @@
+
+public class Chicken extends Bird {
+
+	public Chicken(int age, String gender, int weightInLbs) {
+		super(age, gender, weightInLbs);
+	}
+	
+	// overriding the method defined in Bird
+	public void fly() {
+		System.out.println("not able to fly...");
+	}
+
+}
---
+++ practice/Java/OOD/8.Method Overriding/Zoo.java
@@ -0,0 +1,10 @@
+
+public class Zoo {
+	
+	public static void main(String[] args) {
+		
+		Chicken chicken1 = new Chicken(2, "M", 6);
+		chicken1.fly();
+	}
+	
+}
