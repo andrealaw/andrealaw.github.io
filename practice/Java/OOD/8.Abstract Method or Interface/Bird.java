422bc5ba23f1e1606bb2796e4bdaf7ccbbdce9f7
hkandrealaw <hkandrealaw@gmail.com>
2019-03-23 17:21:34 +1100

    Add Java abstract method sample code

---
+++ practice/Java/OOD/8.Abstract Method or Interface/Bird.java
@@ -0,0 +1,8 @@
+
+public class Bird extends Animal {
+
+	public Bird(int age, String gender, int weightInLbs) {
+		super(age, gender, weightInLbs);
+	}
+	
+}
---
+++ practice/Java/OOD/8.Abstract Method or Interface/Zoo.java
@@ -0,0 +1,10 @@
+
+public class Zoo {
+	
+	public static void main(String[] args) {
+		
+		Sparrow sparrow1 = new Sparrow(2, "F", 5);
+		sparrow1.fly();
+	}
+	
+}
